6aa04c7cc6a74f211e2f083f43b85a61e24527a1
mattxd00 <mattxd00@gmail.com>
2022-07-16 20:29:02 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const dgram = require('node:dgram');
+
+const socket = dgram.createSocket({ type: 'udp4', reuseAddr: true });
+socket.bind(5010);
+
+socket.on('message', (msg, rinfo) => {
+	console.log(msg);
+});
+
+socket.on('listening', () => {
+	console.log(`Listening on ${socket.address().address}:${socket.address().port}`);
+	socket.addMembership('239.255.50.10');
+});
