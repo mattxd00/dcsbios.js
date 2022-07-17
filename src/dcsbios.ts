fa2a3b2ebe282300537ea5c29683216e6da051b9
mattxd00 <mattxd00@gmail.com>
2022-07-17 21:23:16 +0100

    continued structure setup

---
+++ src/dcsbios.ts
@@ -0,0 +1,17 @@
+const dgram = require('node:dgram');
+
+
+
+
+
+const socket = dgram.createSocket({ type: 'udp4', reuseAddr: true });
+socket.bind(5010);
+
+socket.on('message', (msg: any, rinfo: any) => {
+	console.log(msg);
+});
+
+socket.on('listening', () => {
+	console.log(`Listening on ${socket.address().address}:${socket.address().port}`);
+	socket.addMembership('239.255.50.10');
+});
