5fb6be00439889cfbe312539a4bc664eba87900d
mattxd00 <mattxd00@gmail.com>
2022-07-18 04:58:50 +0100

    began making class

--- dist/dcsbios.js
+++ dist/dcsbios.js
@@ -1,11 +1,63 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 const dgram = require('node:dgram');
+const { EventEmitter } = require('node:events');
+/** The main client for interacting
+ * @extends EventEmitter
+ */
+class Client extends EventEmitter {
+    options = {
+        address: '239.255.50.10',
+        receivePort: 5010,
+        sendPort: 7778
+    };
+    _socket;
+    /**
+     * The options a client has been instantiated with
+     * @param {ClientOptions} options
+     */
+    constructor(options) {
+        super();
+        Object.assign(this.options, options);
+        this._socket = this._createSocket();
+        this._beginListen();
+    }
+    /** Creates a dgram socket and stores it
+     * @private
+    */
+    _createSocket() {
+        const socket = dgram.createSocket({ type: 'udp4', reuseAddr: true });
+        socket.bind(this.options.receivePort);
+        return socket;
+    }
+    _beginListen() {
+        return new Promise((resolve, reject) => {
+            this._socket.on('listening', () => {
+                const addr = this._socket.address();
+                console.log(`Socket listening on ${addr.address}:${addr.port}`);
+                this._socket.addMembership(this.options.address);
+                this._socket.on('message', (msg, rinfo) => {
+                    console.log(msg);
+                });
+                resolve(true);
+            });
+        });
+    }
+}
+const client = new Client();
+console.log(client.options);
+/*
+
 const socket = dgram.createSocket({ type: 'udp4', reuseAddr: true });
 socket.bind(5010);
-socket.on('message', (msg, rinfo) => {
+
+socket.on('message', (msg: any, rinfo: any) => {
     console.log(msg);
 });
+
 socket.on('listening', () => {
     console.log(`Listening on ${socket.address().address}:${socket.address().port}`);
     socket.addMembership('239.255.50.10');
 });
+
+*/ 
--- src/dcsbios.ts
+++ src/dcsbios.ts
@@ -1,17 +1,69 @@
+import { Socket } from "dgram";
 const dgram = require('node:dgram');
+const { EventEmitter } = require('node:events');
 
+/** Interface for client options object
+ *  @interface ClientOptions
+ */
+interface ClientOptions {
+	address?: string;
+	sendPort?: number;
+	receivePort?: number;
+}
 
+/** The main client for interacting 
+ * @extends EventEmitter
+ */
+class Client extends EventEmitter  {
+	public readonly options: ClientOptions = {
+		address: '239.255.50.10',
+		receivePort: 5010,
+		sendPort: 7778
+	};
+	/**
+	 * The options a client has been instantiated with
+	 * @param {ClientOptions} options
+	 */
+	constructor(options?: ClientOptions) {
+		super();
 
+		Object.assign(this.options, options);
 
+		this._socket = this._createSocket();
 
-const socket = dgram.createSocket({ type: 'udp4', reuseAddr: true });
-socket.bind(5010);
+		this._beginListen();
 
-socket.on('message', (msg: any, rinfo: any) => {
-	console.log(msg);
-});
+	}
+	/** Creates a dgram socket and stores it
+	 * @private
+	*/
+	private _createSocket(): Socket {
+		const socket = dgram.createSocket({ type: 'udp4', reuseAddr: true });
+		socket.bind(this.options.receivePort);
+		return socket;
+	}
+	private _beginListen(): Promise<boolean> {
+		return new Promise((resolve, reject) => {
+			this._socket.on('listening', () => {
+				const addr = this._socket.address();
+				console.log(`Socket listening on ${addr.address}:${addr.port}`);
+				this._socket.addMembership(this.options.address!);
+				this._socket.on('message', (msg: any, rinfo: any) => {
+					this._decode(msg);
+				})
+				resolve(true);
+			});
+		});
+	}
+	private _decode(buffer: Buffer): void {
+		const controlAddr: any = buffer.readUInt16LE();
+		const count: any = buffer.readUInt16LE();
+		
+		console.log(buffer.toString('hex'));
+		console.log(controlAddr, count);
+	}
+}
 
-socket.on('listening', () => {
-	console.log(`Listening on ${socket.address().address}:${socket.address().port}`);
-	socket.addMembership('239.255.50.10');
-});
+const client = new Client();
+
+console.log(client.options);
